7b54ac9f691b0f0637b88a7b06d286597dd18faa
kujtimsaliu <kujtimsaliu011@gmail.com>
2024-06-13 19:33:45 +0200

    initial commit

--- main.py
+++ main.py
@@ -132,9 +132,6 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
 async def read_users_me(current_user: User = Depends(get_current_active_user)):
     return current_user
 
-
 @app.get("/users/me/items")
 async def read_own_items(current_user: User = Depends(get_current_active_user)):
     return [{"item_id": 1, "owner": current_user}]
-
-
